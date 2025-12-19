fcff4ade807be40dbcc2a863d2154a9d2764cc17
Samuel Ferreira <samuelferreiraj6@gmail.com>
2025-12-19 13:56:41 -0300

    chore: configura rotas de auth no app e atualiza variaveis de exemplo

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Montar as rotas
+app.use('/api/auth', authRoutes);
 
 // Rotas
 app.get('/', (req, res) => {
